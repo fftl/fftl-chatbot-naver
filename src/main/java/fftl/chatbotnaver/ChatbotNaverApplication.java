2e02cf87fae88a5b3720a7725c24b9c52bdaacc7
fftl <fftl.0710@gmail.com>
2022-05-30 23:10:09 +0900

    update: main

---
+++ src/main/java/fftl/chatbotnaver/ChatbotNaverApplication.java
@@ -0,0 +1,13 @@
+package fftl.chatbotnaver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatbotNaverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatbotNaverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fftl/chatbotnaver/ChatbotNaverApplicationTests.java
@@ -0,0 +1,13 @@
+package fftl.chatbotnaver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatbotNaverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
